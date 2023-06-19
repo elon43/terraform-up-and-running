852815084ee6d26c8b2130692a277f306bccd11f
Stefan Thompson <elon43@yahoo.com>
2023-06-19 19:13:52 +0000

    Ch4: Turned Web Server Cluster into a Module and Create a Stage and Production Directory That Calls the Module

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -1,22 +1,11 @@
-# Configure the AWS Provider
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs
-
-provider "aws" {
-  region = "us-east-2"
-}
-
-# Create Terraform Backend using S3
-# https://developer.hashicorp.com/terraform/language/settings/backends/s3
-terraform {
-  backend "s3" {
-    bucket = "terraform-up-and-running-state-sct6443"
-    key    = "stage/services/webserver-cluster/terraform.tfstate"
-    region = "us-east-2"
-
-
-    dynamodb_table = "terraform-up-and-running-locks"
-    encrypt = true
-  }
+# Declare some local variables
+# https://developer.hashicorp.com/terraform/language/values/locals
+locals {
+  http_port    = 80
+  any_port     = 0
+  any_protocol = "-1"
+  tcp_protocol = "tcp"
+  all_ips      = ["0.0.0.0/0"]
 }
 
 # Retrieve Information from the Remote State Data Source
@@ -25,8 +14,8 @@ data "terraform_remote_state" "db" {
   backend = "s3"
 
   config = {
-    bucket = "terraform-up-and-running-state-sct6443"
-     key    = "stage/data-stores/mysql/terraform.tfstate"
+    bucket = var.db_remote_state_bucket
+    key    = var.db_remote_state_key
     region = "us-east-2"
   }
 }
@@ -36,15 +25,15 @@ data "terraform_remote_state" "db" {
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/launch_configuration
 resource "aws_launch_configuration" "example" {
   image_id        = "ami-0430580de6244e02e"
-  instance_type   = "t2.micro"
+  instance_type   = var.instance_type
   security_groups = [aws_security_group.instance.id]
-  
+
   # Create Templatefile
   # https://developer.hashicorp.com/terraform/language/functions/templatefile
-  user_data = templatefile("user-data.sh", {
+  user_data = templatefile("${path.module}/user-data.sh", {
     server_port = var.server_port
-    db_address = data.terraform_remote_state.db.outputs.address
-    db_port = data.terraform_remote_state.db.outputs.port
+    db_address  = data.terraform_remote_state.db.outputs.address
+    db_port     = data.terraform_remote_state.db.outputs.port
   })
 
   lifecycle {
@@ -61,12 +50,12 @@ resource "aws_autoscaling_group" "example" {
   target_group_arns = [aws_lb_target_group.asg.arn]
   health_check_type = "ELB"
 
-  min_size = 2
-  max_size = 10
+  min_size = var.min_size
+  max_size = var.max_size
 
   tag {
     key                 = "Name"
-    value               = "terraform-asg-example"
+    value               = "${var.cluster_name}-example"
     propagate_at_launch = true
   }
 }
@@ -89,20 +78,24 @@ data "aws_subnets" "default" {
 # Create Security Group 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
 resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
+  name = "${var.cluster_name}-instance"
+}
 
-  ingress {
-    from_port   = var.server_port
-    to_port     = var.server_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# Define the Ingress and Egress Rules
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group_rule
+resource "aws_security_group_rule" "example" {
+  type              = "ingress"
+  from_port         = var.server_port
+  to_port           = var.server_port
+  protocol          = local.tcp_protocol
+  cidr_blocks       = local.all_ips
+  security_group_id = aws_security_group.instance.id
 }
 
 # Create Application Load Balancer
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb
 resource "aws_lb" "example" {
-  name               = "terraform-asg-example"
+  name               = "${var.cluster_name}-example"
   load_balancer_type = "application"
   subnets            = data.aws_subnets.default.ids
   security_groups    = [aws_security_group.alb.id]
@@ -112,7 +105,7 @@ resource "aws_lb" "example" {
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb_listener
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.example.arn
-  port              = "80"
+  port              = local.http_port
   protocol          = "HTTP"
 
   default_action {
@@ -129,7 +122,7 @@ resource "aws_lb_listener" "http" {
 # Create Load Balancer Target Group
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb_target_group
 resource "aws_lb_target_group" "asg" {
-  name     = "terraform-asg-example"
+  name     = "${var.cluster_name}-example"
   port     = var.server_port #traffic comes in on the lb port 80, application running on 8080
   protocol = "HTTP"
   vpc_id   = data.aws_vpc.default.id
@@ -166,19 +159,27 @@ resource "aws_lb_listener_rule" "asg" {
 # Create Security Group for Load Balancer
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
 resource "aws_security_group" "alb" {
-  name = "terraform-example-alb"
+  name = "${var.cluster_name}-alb"
+}
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# Define the Ingress and Egress Rules
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group_rule
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type              = "ingress"
+  security_group_id = aws_security_group.alb.id
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  from_port   = local.http_port
+  to_port     = local.http_port
+  protocol    = local.tcp_protocol
+  cidr_blocks = local.all_ips
+}
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type              = "egress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port   = local.any_port
+  to_port     = local.any_port
+  protocol    = local.any_protocol
+  cidr_blocks = local.all_ips
 }
