326279964564f33040bf05c81c49142c08436e3f
Stefan Thompson <elon43@yahoo.com>
2023-06-21 17:04:38 +0000

    End of Ch5: Implemented zero downtime deployment ofor web server cluster module.  Also, implemented conditional expressions

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -24,7 +24,7 @@ data "terraform_remote_state" "db" {
 # Create Launch Configuration
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/launch_configuration
 resource "aws_launch_configuration" "example" {
-  image_id        = "ami-0430580de6244e02e"
+  image_id        = var.ami
   instance_type   = var.instance_type
   security_groups = [aws_security_group.instance.id]
 
@@ -34,6 +34,7 @@ resource "aws_launch_configuration" "example" {
     server_port = var.server_port
     db_address  = data.terraform_remote_state.db.outputs.address
     db_port     = data.terraform_remote_state.db.outputs.port
+    server_text = var.server_text
   })
 
   lifecycle {
@@ -44,6 +45,8 @@ resource "aws_launch_configuration" "example" {
 # Create Autoscaling Group
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/autoscaling_group
 resource "aws_autoscaling_group" "example" {
+  name = var.cluster_name
+
   launch_configuration = aws_launch_configuration.example.name
   vpc_zone_identifier  = data.aws_subnets.default.ids
 
@@ -53,12 +56,57 @@ resource "aws_autoscaling_group" "example" {
   min_size = var.min_size
   max_size = var.max_size
 
+  instance_refresh {
+    strategy = "Rolling"
+
+    preferences {
+      min_healthy_percentage = 50
+    }
+  }
+  
   tag {
     key                 = "Name"
     value               = "${var.cluster_name}-example"
     propagate_at_launch = true
   }
+
+  # Create a dynamic block
+  # https://developer.hashicorp.com/terraform/language/expressions/dynamic-blocks
+  dynamic "tag" {
+    for_each = var.custom_tags
+    content {
+      key                 = tag.key
+      value               = tag.value
+      propagate_at_launch = true
+    }
+  }
 }
+
+# Create Conditional Autoscaling Schedule
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/autoscaling_schedule
+resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
+  count = var.enable_autoscaling ? 1 : 0
+
+  scheduled_action_name  = "${var.cluster_name}-scale_out_during_business_hours"
+  min_size               = 2
+  max_size               = 10
+  desired_capacity       = 10
+  recurrence             = "0 9 * * *"
+  autoscaling_group_name = aws_autoscaling_group.example.name
+}
+
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+  count = var.enable_autoscaling ? 1 : 0
+
+  scheduled_action_name  = "${var.cluster_name}-scale_in_at_night"
+  min_size               = 2
+  max_size               = 10
+  desired_capacity       = 2
+  recurrence             = "0 17 * * *"
+  autoscaling_group_name = aws_autoscaling_group.example.name
+}
+
+
 
 # Retrieve Default VPC
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/vpc
