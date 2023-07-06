bc79c5d062de8500f999f0166b64ca32ace17c87
Stefan Thompson <elon43@yahoo.com>
2023-07-06 00:34:31 +0000

    Added verfification code to the stage and prod directories that tested the webserver modules 1-6.  This is the end of chapter 6

---
+++ modules/data-stores/mysql/main.tf
@@ -0,0 +1,31 @@
+# Configure the Required Providers Block
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.4.0"
+    }
+  }
+}
+
+# Create MYSQL Database
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/db_instance
+resource "aws_db_instance" "example" {
+  identifier_prefix   = "terraform-up-and-running"
+  allocated_storage   = 10
+  instance_class      = "db.t2.micro"
+  skip_final_snapshot = true
+
+  # Enable backups
+  backup_retention_period = var.backup_retention_period
+  
+  #If specified, this DB will be a replica
+  replicate_source_db     = var.replicate_source_db
+
+  # Only sset these params if replicate_resource_db is not set
+  engine              = var.replicate_source_db == null ? "mysql" : null
+  db_name             = var.replicate_source_db == null ? var.db_name : null 
+  username            = var.replicate_source_db == null ? var.db_username : null
+  password            = var.replicate_source_db == null ? var.db_password : null
+}
